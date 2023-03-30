04f72b3ad0d8c07f8982d6e66a85457ab74495a9
Jamal MAARAD <jamal.maarad@es.nestle.com>
2023-03-30 12:27:24 +0000

    Middleware concept

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,24 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res) => {
+app.use((req, res, next) => {
+    console.log('Requete reçue!');
+    next();
+});
+
+app.use((req, res, next) => {
+    res.status(201);
+    next();
+});
+
+app.use((req, res, next) => {
     res.json({ message: 'votre requete a bien ete reçue !' });
+    next();
+});
+
+app.use((req, res, next) => {
+    console.log('Reponse envoyé!');
+    next();
 });
 
 module.exports = app;
