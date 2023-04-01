dec39cc5899029e27fe439e1a4de7dffe3c80992
Jamal MAARAD <jamal.maarad@es.nestle.com>
2023-04-01 22:25:52 +0000

    Using Mongoose to connect to databse and perform GET (find,findone) & POST

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,15 @@
 const express = require('express');
-
+const mongoose = require("mongoose");
 const app = express();
+const Thing = require("./models/thing");
 
+
+mongoose.connect('mongodb+srv://root:toor@cluster0.sectcuz.mongodb.net/?retryWrites=true&w=majority', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
 // //Express prend toutes les requêtes qui ont comme Content-Type  application/json  
 // et met à disposition leur  body  directement sur l'objet req, 
 // ce qui nous permet d'écrire le middleware POST
@@ -14,36 +22,63 @@ app.use((req, res, next) => {
     next();
 });
 
+
+
+
 //Middleware GET
 
+// app.get('/api/stuff', (req, res, next) => {
+//     const stuff = [{
+//             _id: 'oeihfzeoi',
+//             title: 'Mon premier objet',
+//             description: 'Les infos de mon premier objet',
+//             imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+//             price: 4900,
+//             userId: 'qsomihvqios',
+//         },
+//         {
+//             _id: 'oeihfzeomoihi',
+//             title: 'Mon deuxième objet',
+//             description: 'Les infos de mon deuxième objet',
+//             imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+//             price: 2900,
+//             userId: 'qsomihvqios',
+//         },
+//     ];
+//     res.status(200).json(stuff);
+// });
+
+//Middleware utilisation mongoose
 app.get('/api/stuff', (req, res, next) => {
-    const stuff = [{
-            _id: 'oeihfzeoi',
-            title: 'Mon premier objet',
-            description: 'Les infos de mon premier objet',
-            imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-            price: 4900,
-            userId: 'qsomihvqios',
-        },
-        {
-            _id: 'oeihfzeomoihi',
-            title: 'Mon deuxième objet',
-            description: 'Les infos de mon deuxième objet',
-            imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-            price: 2900,
-            userId: 'qsomihvqios',
-        },
-    ];
-    res.status(200).json(stuff);
+    Thing.find()
+        .then(things => { console.log(things); return res.status(200).json(things) })
+        .catch(error => res.status(400).json({ error }));
 });
 
-// Middleware POST
-app.post('/api/stuff', (req, res, next) => {
-    console.log(req.body);
-    res.status(201).json({
-        message: 'Objet créé !'
-    });
+app.get('/api/stuff/:id', (req, res, next) => {
+    Thing.findOne({ _id: req.params.id })
+        .then(thing => res.status(200).json(thing))
+        .catch(error => res.status(404).json({ error }));
 });
+
+//Utilisisation de monogoose et remplacant la method post
+app.post('/api/stuff', (req, res, next) => {
+    delete req.body._id;
+    const thing = new Thing({
+        ...req.body
+    });
+    thing.save()
+        .then(() => res.status(201).json({ message: 'Objet enregistré !' }))
+        .catch(error => res.status(400).json({ error }));
+});
+
+// // Middleware POST
+// app.post('/api/stuff', (req, res, next) => {
+//     console.log(req.body);
+//     res.status(201).json({
+//         message: 'Objet créé !'
+//     });
+// });
 
 //Middleware example
 // app.use((req, res, next) => {
